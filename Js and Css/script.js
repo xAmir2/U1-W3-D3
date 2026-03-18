71d66fda5c54a96a400301b71b497bb929f2d37b
Amir B. Kahna <amir.kahna2@gmail.com>
2026-03-18 16:05:40 +0100

    Adding To-do list

---
+++ Js and Css/script.js
@@ -0,0 +1,30 @@
+const formSubmit = (e) => {
+  e.preventDefault();
+  const itemInput = document.getElementById("todolist");
+  const item = itemInput.value;
+  const grocery = document.createElement("article");
+  grocery.classList.add("groceryItem");
+  const section = document.getElementById("saveditems");
+  section.innerHTML += `
+  <article class="groceryItem">
+  <p>${item}</p>
+  <button onclick="deleteGroceryItem(event)"> Delete</button>
+  <button onclick="slashItem(event)"> Marks as complete</button>
+  </article>
+  `;
+  form.reset();
+};
+
+const deleteGroceryItem = (e) => {
+  const button = e.target;
+  const article = button.parentElement;
+  article.remove();
+};
+const form = document.getElementById("formtodolist");
+form.addEventListener("submit", formSubmit);
+
+const slashItem = (e) => {
+  const button = e.target;
+  const article = button.parentElement;
+  article.style.textDecoration = "line-through";
+};
